a98dee89550feee1b55a403b54b5b3a833f022c3
Sriram <31518313+sriramrex@users.noreply.github.com>
2024-12-17 20:31:53 +0100

    Create Polar_FT_Plot.m
    This the code for visualizing the planer forces (x,y) in polar coordinate when the tool is running in ideal condition.

---
+++ Matlab_files/Polar_FT_Plot.m
@@ -0,0 +1,93 @@
+clear; close all; clc
+
+base_path = 'C:\IIT_HHCM_LAB\Matlab_Files\FTSensor0_streamingData_25_11_2024_14_20_14.csv';
+ftsensor = readtable(base_path);
+sensor_t = ftsensor.SampleID;
+sensor_data = [ftsensor.Fx'; ftsensor.Fy'];
+
+%% Compute the polar plot for the x,y forces
+
+time_stamp = linspace(0,15000,length(sensor_t));
+time = time_stamp/1000;
+period = 15; % Time for one complete cycle (2*pi)
+% Calculate angles
+time_angle = 2 * pi * mod(time, period) / period;
+
+% force values 
+
+f_x = ftsensor.Fx';
+f_y = ftsensor.Fy';
+
+% Calculate angles
+angles = atan2(f_y,f_x);
+% angles(angles < 0) = angles(angles < 0) + 2*pi;
+ theta = mod(angles,360);
+ theta = round(theta,2);
+
+% Calculate  euclidien distance
+distance = sqrt(f_x.^2 + f_y.^2);
+
+
+%% 3d data
+
+x = distance .*cos(angles);
+y = distance .*sin(angles);
+
+x_t = distance .*cos(time_angle);
+y_t = distance .*sin(time_angle);
+
+%% Plot distance vs angle
+figure;
+polarscatter(angles, distance,"filled"); % Polar plot for distance as a function of angle
+title('Distance vs Angle');
+
+figure(2);
+
+scatter3(x,y,time,36,time,"filled");
+colormap jet;
+colorbar;
+% Label the axes
+xlabel('X (Radius * cos(Angle))');
+ylabel('Y (Radius * sin(Angle))');
+zlabel('Time');
+title('3D Polar Plot with Radius, Angle, and Time');
+
+figure(3);
+
+polarscatter(time_angle, distance,"filled"); % Polar plot for distance as a function of angle
+title('Distance vs Angle');
+
+figure(4);
+
+scatter3(x_t,y_t,time,36,time,"filled");
+colormap jet;
+colorbar;
+% Label the axes
+xlabel('X_time ');
+ylabel('Y_time ');
+zlabel('Time');
+title('3D Polar Plot with Radius, Angle, and Time')
+
+figure (5);
+
+subplot(3,1,1);
+plot(time, distance,'b','LineWidth',2);
+xlabel('time');
+ylabel('radius');
+
+subplot(3,1,2);
+plot(time, angles,'g','LineWidth',2);
+xlabel('time');
+ylabel('angle (rad)');
+
+subplot(3,1,3);
+plot(time,time_angle,'r','LineWidth',2);
+xlabel('time');
+ylabel('time @ angle (rad)');
+
+
+
+
+
+
+
